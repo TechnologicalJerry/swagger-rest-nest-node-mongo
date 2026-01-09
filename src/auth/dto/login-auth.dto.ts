19d0b4e0b52eede91590667ea40c1fef5b86243c
Rajat Verma <cserajatverma@gmail.com>
2026-01-09 12:21:59 +0530

    Format ApiProperty decorator in LoginAuthDto
    Reformatted the ApiProperty decorator in LoginAuthDto for improved readability by using multiline object notation.

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginAuthDto {
-  @ApiProperty({ description: 'The identifier of the user (email or username)' })
+  @ApiProperty({
+    description: 'The identifier of the user (email or username)',
+  })
   @IsString()
   @IsNotEmpty()
   identifier: string;
