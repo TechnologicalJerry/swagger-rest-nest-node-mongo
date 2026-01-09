303d230de67a135c577c5bdc179c0251716d1c36
Rajat Verma <cserajatverma@gmail.com>
2026-01-09 12:21:54 +0530

    Format ApiProperty decorator in ForgotPasswordDto
    Reformatted the ApiProperty decorator in ForgotPasswordDto for improved readability by using a multi-line object.

--- src/auth/dto/forgot-password.dto.ts
+++ src/auth/dto/forgot-password.dto.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class ForgotPasswordDto {
-  @ApiProperty({ description: 'The email or username of the user to reset the password' })
+  @ApiProperty({
+    description: 'The email or username of the user to reset the password',
+  })
   @IsString()
   @IsNotEmpty()
   identifier: string;
